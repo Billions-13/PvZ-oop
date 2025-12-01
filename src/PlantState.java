30af67d8c800c304865b035b3ca8e3178cc9446d
Billions-13 <trannhoc0104@gmail.com>
2025-12-01 23:04:26 +0700

    create fold

--- src/PlantState.java
+++
@@ -1,8 +0,0 @@
-public enum PlantState {
-    SPAWNING,   // mới đặt xuống
-    IDLE,       // đứng chờ
-    ATTACKING,  // đang tấn công
-    COOLDOWN,   // đang chờ hồi chiêu
-    DYING,      // đang chơi animation chết
-    DEAD        // đã chết hẳn
-}
--- src/SunProductionBehavior.java
+++
@@ -1,3 +0,0 @@
-public interface SunProductionBehavior {
-    void updateSunProduction(Sunflower source, double currentTime);
-}
